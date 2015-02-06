2c3641b14a45e8ef0cb9b67a0ed6cc5af6e5155f
Praphull Kumar <praphull27@gmail.com>
2015-02-05 17:43:05 -0800

    Commit

--- removeLessFrequentWords.py
+++ removeLessFrequentWords.py
@@ -1,4 +1,3 @@
-import re
 from collections import OrderedDict
 
 word_dict = {}
@@ -12,42 +11,32 @@ with open("nyt_corpus_without_stopwords.txt") as infile:
     		else:
     			word_dict[word] = 1
 
-print len(word_dict.keys())
-
 word_dict = OrderedDict(sorted(word_dict.items(), key=lambda t: t[1], reverse=True))
-words_less_frequent = []
 
 out_handle_filtered_count = open("nyt_corpus_without_stopwords_count.txt", 'wb')
-
 for item in word_dict.items():
 	if item[1] > 5:
 		out_handle_filtered_count.write(item[0] + ' : ' + str(item[1]) + '\n')
-	else:
-		words_less_frequent.append(item[0])
 
 out_handle_filtered_count.close()
 
-print len(words_less_frequent)
+def remove_less_frequent_words (line):
+	line = line.strip()
+	line_tokenize = line.split()
+	line_tokenize = [word for word in line_tokenize if word_dict[word] > 5]
+	return " ".join(line_tokenize)
 
-# pattern = re.compile(r'\b(' + r'|'.join(words_less_frequent) + r')\b\s*')
+out_handle = open("nyt_corpus_cleaned_for_lda.txt", 'wb')
+line_count = 0
+with open("nyt_corpus_without_stopwords.txt") as infile:
+    for line in infile:
+    	out = remove_less_frequent_words(line)
+        out_handle.write(out + "\n")
+        line_count += 1
+        if line_count % 1000000 == 0:
+        	print line_count
+        else:
+        	if line_count % 100000 == 0:
+        		print "."
 
-# def remove_less_frequent_words (line):
-# 	line = line.strip()
-# 	new_line = pattern.sub('', line)
-# 	new_line = re.sub(r'\s\s+', ' ', new_line)
-# 	return new_line.strip()
-
-# out_handle = open("nyt_corpus_cleaned_for_lda.txt", 'wb')
-# line_count = 0
-# with open("nyt_corpus_without_stopwords.txt") as infile:
-#     for line in infile:
-#     	out = remove_less_frequent_words(line)
-#         out_handle.write(out + "\n")
-#         line_count += 1
-#         if line_count % 100000 == 0:
-#         	print line_count
-#         else:
-#         	if line_count % 10000 == 0:
-#         		print "."
-
-# out_handle.close()
+out_handle.close()
